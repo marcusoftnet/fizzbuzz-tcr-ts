96423b108a6cf2839610dab20430f0ab3051f9ef
Marcus Hammarberg <marcusoft.net@gmail.com>
2025-10-29 14:49:18 +0100

    TCR @ Wed Oct 29 14:49:18 2025

--- index.test.ts
+++ index.test.ts
@@ -2,9 +2,13 @@ import { describe, it, expect } from "vitest";
 import { single } from ".";
 
 describe("FizzBuzz single number", () => {
+  const asserSingle = (input: number, expected: string) => {
+    const result = single(input);
+    expect(result).toBe(expected);
+  };
+
   it('returns "1" for 1', () => {
-    const result = single(1);
-    expect(result).toBe("1");
+    asserSingle(1, "1");
   });
 
   it('returns "2" for 2', () => {
