ab281d15708b918455b48338042b08b52c04077f
Marcus Hammarberg <marcusoft.net@gmail.com>
2025-10-29 14:52:18 +0100

    TCR @ Wed Oct 29 14:52:18 2025

--- index.test.ts
+++ index.test.ts
@@ -9,4 +9,5 @@ describe("FizzBuzz single number", () => {
   it('returns "1" for 1', () => assertSingle(1, "1"));
   it('returns "2" for 2', () => assertSingle(2, "2"));
   it('returns "3" for Fizz', () => assertSingle(3, "Fizz"));
+  it('returns "3" for Buzz', () => assertSingle(5, "Buzz"));
 });
--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
 export const single = (n: number): string => {
+  if(n === 5) return "Buzz";
   if(n === 3) return "Fizz";
   return n.toString();
 };
