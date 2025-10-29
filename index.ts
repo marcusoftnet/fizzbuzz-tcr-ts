dc5e05a6d9aaaf09dcf56515b2fc6b5a0b023cb3
Marcus Hammarberg <marcusoft.net@gmail.com>
2025-10-29 15:00:02 +0100

    TCR @ Wed Oct 29 15:00:02 2025

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 export const single = (n: number): string => {
-  if(n % 5 === 0) return "Buzz";
-  if(n % 3 === 0) return "Fizz";
+  if (n % 3 === 0 && n % 5 === 0) return "FizzBuzz";
+  if (n % 5 === 0) return "Buzz";
+  if (n % 3 === 0) return "Fizz";
   return n.toString();
 };
